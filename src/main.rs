8bda4bc95b6380de7b86209be447a7e1cb07783c
XNeyMo <xneymodev@gmail.com>
2023-09-15 08:24:53 -0500

    Other template with IMPL

--- src/main.rs
+++ src/main.rs
@@ -23,20 +23,33 @@ struct LanguageData {
     kanji: Vec<JapaneseGroup>,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut file = File::open("data/words.json")?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
+impl LanguageData {
+    fn run() -> Option<(Vec<JapaneseGroup>, Vec<JapaneseGroup>, Vec<JapaneseGroup>)> {
+        let mut file = File::open("data/words.json").ok()?;
+        let mut contents = String::new();
+        if file.read_to_string(&mut contents).is_err() {
+            return None;
+        }
 
-    let data: LanguageData = serde_json::from_str(&contents)?;
+        let data: LanguageData = serde_json::from_str(&contents).ok()?;
 
-    let hiragana_groups = &data.hiragana;
-    let katakana_groups = &data.katakana;
-    let kanji_groups = &data.kanji;
+        let hiragana_groups = data.hiragana;
+        let katakana_groups = data.katakana;
+        let kanji_groups = data.kanji;
 
-    println!("{:?}", hiragana_groups);
-    println!("{:?}", katakana_groups);
-    println!("{:?}", kanji_groups);
+        Some((hiragana_groups, katakana_groups, kanji_groups))
+    }
+}
 
-    Ok(())
+fn main() {
+    if let Some((hiragana_groups, katakana_groups, kanji_groups)) = LanguageData::run() {
+        println!("{:?}", hiragana_groups);
+        println!("{:?}", katakana_groups);
+        println!("{:?}", kanji_groups);
+    } 
+    
+    else {
+        eprintln!("Error: Unable to read data.");
+        std::process::exit(1);
+    }
 }
