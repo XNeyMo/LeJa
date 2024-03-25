5f45cd75c28cb4bccd1d0323c659a6185cca6c5d
XNeyMo <xneymodev@gmail.com>
2024-03-24 21:44:46 -0500

    hi

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+#![allow(warnings)]
+
+use serde::Deserialize;
+
+use std::fs::File;
+use std::path::Path;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Char {
+    level: u8,
+    japan: String,
+    latin: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Kana {
+    chars: Vec<Char>,
+}
+
 fn main() {
-    println!("Hello World!");
+    let json_file_path = Path::new("./data/chars.json");
+    let file = File::open(json_file_path).expect("file not found");
+
+    let japanese: Vec<Kana> = serde_json::from_reader(file).unwrap();
 }
